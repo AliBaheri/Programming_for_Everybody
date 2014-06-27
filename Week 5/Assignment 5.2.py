f0d1c3a4c3b78ecca40a9e1249907fdb094a4eb2
Feagurth <Feagurth@gmail.com>
2014-06-27 06:17:26 +0200

    Week 5 Assginments
    Changed Try/Except position.

--- Week 5/Assignment 5.2.py
+++ Week 5/Assignment 5.2.py
@@ -5,21 +5,23 @@
 
 largest = None
 smallest = None
-try:
-    while True:
+while True:
+    try:
         num = raw_input("Enter a number: ")
         if num == "done" : 
             break
 
+        num = int(num)                        
+            
         if largest is None or largest < num:
             largest = num
 
         if smallest is None or smallest > num:
             smallest = num
             
-    num = integer(num)            
-except:
-    print "Invalid input"
+    except:
+        print "Invalid input"
+        continue
         
 print "Maximum is", largest
 print "Minimum is", smallest
